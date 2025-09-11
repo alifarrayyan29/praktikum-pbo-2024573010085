b703fec02391632470b9f3d05af9aa7ef409e53e
alifarrayyan29 <alifarrayyan12345@gmail.com>
2025-09-11 15:34:56 +0700

    Inisialisasi 1

---
+++ src/modul_1/Main.java
@@ -0,0 +1,8 @@
+package modul_1;
+
+public class Main {
+    // Fungsi khusus sebagai entry point dari Aplikasi yang dibuat.
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
